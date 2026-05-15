9bb1eb5903fb729395c7e1878c669ed525d7b78c
MokhoanaziRO <olgahrefiloe6@gmail.com>
2026-05-15 04:12:03 +0200

    Create morse code.cpp
    the introduction of the morse code

---
+++ morse code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <cctype>
+
+using namespace std;
+
+// Morse code mapping for letters A-Z
+map<char, string> morseCode = {
+    {'A', ".-"},   {'B', "-..."}, {'C', "-.-."}, {'D', "-.."},  {'E', "."},
+    {'F', "..-."}, {'G', "--."},  {'H', "...."}, {'I', ".."},   {'J', ".---"},
+    {'K', "-.-"},  {'L', ".-.."}, {'M', "--"},   {'N', "-."},   {'O', "---"},
+    {'P', ".--."}, {'Q', "--.-"}, {'R', ".-."},  {'S', "..."},  {'T', "-"},
+    {'U', "..-"},  {'V', "...-"}, {'W', ".--"},  {'X', "-..-"}, {'Y', "-.--"},
+    {'Z', "--.."},
+    {' ', " "} // Space between words
+}
+
+    return 0;
+}
